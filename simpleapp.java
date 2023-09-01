547e4052ab37da109220896085168acc29d64bfa
Osura Rathnayake <osuranew@gmail.com>
2023-09-01 14:27:40 +0800

    Update simpleapp.java

--- simpleapp.java
+++ simpleapp.java
@@ -1,7 +1,7 @@
 public class hello{
   public static void main(String [] args){
     
-    for (int i-1; i<=10; i++){
+    for (int i=1; i<=10; i++){
       system.out.orintln("hello world"+i);
     }
   }
